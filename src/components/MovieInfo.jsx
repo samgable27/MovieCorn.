6b4a1aaf38f3622b44ce773737fbda981a357ee4
Sam <samarooniii@yahoo.com>
2023-02-03 17:52:08 -0500

    now done

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -16,19 +16,29 @@ export const MovieInfo = ({ selectedMovie, setSelectedMovie, isLoading }) => {
   }, [selectedMovie]);
 
   return (
-    <div className="flex flex-row p-15 justify-center shadow-xl">
-      <div className="flex items-center mx-auto mt-6">
-        <img className="rounded-md" src={movieInfo.Poster} alt="" />
-        <div className="m-16">
-          <MovieInfoCard isLoading={isLoading} movieInfo={movieInfo} />
-          <div
-            onClick={() => setSelectedMovie()}
-            className="text-gray-500 cursor-pointer "
-          >
-            <ExpandLessIcon className="w-10 h-10 hover:scale-110 transition transform duration-150 ease-out focus:border-white" />
+    <>
+      {isLoading ? (
+        <div>
+          <h1 className="text-4xl text-white font-bold">
+            Loading movie details...
+          </h1>
+        </div>
+      ) : (
+        <div className="flex flex-row p-15 justify-center shadow-xl">
+          <div className="flex items-center mx-auto mt-6">
+            <img className="rounded-md" src={movieInfo.Poster} alt="" />
+            <div className="m-16">
+              <MovieInfoCard movieInfo={movieInfo} />
+              <div
+                onClick={() => setSelectedMovie()}
+                className="text-gray-500 cursor-pointer "
+              >
+                <ExpandLessIcon className="w-10 h-10 hover:scale-110 transition transform duration-150 ease-out focus:border-white" />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
